4f53819bff821f1b0ffcae1d0fd6a5e6a83f447f
Katia Chable <zs24016721@estudiantes.uv.mx>
2025-08-28 15:27:05 -0600

    now user input its repeated 3 times

--- neww.java
+++ neww.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Ingresa un valor: ");
-        int valor = sc.nextInt();
-        System.out.println("El valor ingresado es: " + valor);
+
+        for (int i = 1; i <= 3; i++) {
+            System.out.print("Ingresa un valor (" + i + "): ");
+            int valor = sc.nextInt();
+            System.out.println("El valor ingresado es: " + valor);
+        }
     }
 }
