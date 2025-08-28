bc566cee0fa39fff42dfe9fc1faf69fca23c6e41
Katia Chable <zs24016721@estudiantes.uv.mx>
2025-08-28 14:41:32 -0600

    algorithm to take and show user input

---
+++ README.md
@@ -0,0 +1,9 @@
+# Mi second repo
+
+halo hi hello konichiwa estoy creating mi second repo en la clase de programación orientada a objects!! wooho
+
+## como ejecutarlo
+# compila el programa
+newww.java
+# ejecuta el programa
+new 
---
+++ neww.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingresa un valor: ");
+        int valor = sc.nextInt();
+        System.out.println("El valor ingresado es: " + valor);
+    }
+}
